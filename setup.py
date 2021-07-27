4c6038fa40a4a9432e1624950259b4ff46905c94
Moses Chawawa <moseschawawa@gmail.com>
2021-07-27 12:01:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author_email='se-dmc@bhp.org.bw',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/covid19-vaccine/esr21',
+    url='',
     license='GPL license, see LICENSE',
     description='ESR21',
     long_description=README,
